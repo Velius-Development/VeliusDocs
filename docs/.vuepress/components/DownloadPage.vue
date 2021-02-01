d7407b5dc8722f7baf224c0921d0592b1b6b8524
SpyrexDE <malikfk100@gmail.com>
2021-02-01 13:49:27 +0100

    Updated api links

--- docs/.vuepress/components/DownloadPage.vue
+++ docs/.vuepress/components/DownloadPage.vue
@@ -34,9 +34,11 @@ export default {
             style.setAttribute("id", "styles");
             style.textContent = "html, body, #app, .theme-container, .page{height: 100%; width: 100%; padding: 0;} .theme-default-content, .page-edit {display: none;}";
             document.head.append(style);
-            axios.get("https://mysterymaker.zap106456-1.plesk05.zap-webspace.com/php/getVersion.php").then(response => {
+            axios.get("https://mysterymaker.zap106456-1.plesk05.zap-webspace.com/api/getVersion.php").then(response => {
                 this.version = response.data
+            });
 
+            axios.get("https://mysterymaker.zap106456-1.plesk05.zap-webspace.com/setup.exe").then(response => {
                 $('#downloadBtn').href="https://mysterymaker.zap106456-1.plesk05.zap-webspace.com/setup.exe";
                 this.downloadText = "Download";
             });
