50ed340c164ecc5b3c886716f70abb3c222cbce4
Spyrex <57133330+SpyrexDE@users.noreply.github.com>
2021-04-23 17:56:42 +0200

    Fix DownloadPage.vue

--- docs/.vuepress/components/DownloadPage.vue
+++ docs/.vuepress/components/DownloadPage.vue
@@ -35,12 +35,8 @@ export default {
             style.textContent = "html, body, #app, .theme-container, .page{height: 100%; width: 100%; padding: 0;} .theme-default-content, .page-edit {display: none;}";
             document.head.append(style);
             axios.get("https://mysterymaker.zap106456-1.plesk05.zap-webspace.com/api/getVersion.php").then(response => {
-                this.version = response.data
-<<<<<<< HEAD
-
-=======
+                this.version = response.data;
                 $('#downloadBtn').href="https://mysterymaker.zap106456-1.plesk05.zap-webspace.com/setup.exe";
->>>>>>> cd0153d1fd46100b0102495259611328b7e31247
                 this.downloadText = "Download";
             });
 
